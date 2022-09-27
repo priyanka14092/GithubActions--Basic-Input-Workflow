bceadc6db9d6ddb4dd9b2b7794fc2687ca3883f5
priyanka14092 <priyankabansal582@gmail.com>
2022-09-27 18:28:24 +0530

    Added env

--- add.py
+++ add.py
@@ -5,4 +5,4 @@ lastNum = int(sys.argv[2])
 
 sum = firstNum + lastNum
 
-print(f"Sum of Numbers {firstNum} and {lastNum} is: {sum}")
+print("\n" + f"Sum of Numbers {firstNum} and {lastNum} is: {sum}")
